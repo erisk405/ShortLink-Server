4472aa07d6c12128b97f1948c29f43ac8e3c1da9
erisk405 <133034175+erisk405@users.noreply.github.com>
2025-04-02 17:13:34 +0700

    allowedOrigins

--- src/infrastructure/server.ts
+++ src/infrastructure/server.ts
@@ -9,13 +9,21 @@ import { IpInfoGeoLocationService } from "./ipInfoGeoLocationService";
 dotenv.config();
 const app: Express = express();
 const PORT: number = parseInt(process.env.PORT || "8080", 10);
-
+const allowedOrigins = [process.env.CLIENT_URL, "http://localhost:3000"];
 const urlRepository = new PrismaUrlRepository();
 const geoLocationService = new IpInfoGeoLocationService(process.env.IPINFO_API_KEY || "");
 const urlService = new UrlService(urlRepository, geoLocationService);
 const urlController = new UrlController(urlService);
 
-app.use(cors({ origin: "http://localhost:5173", methods: ["GET", "POST"], allowedHeaders: ["Content-Type"] }));
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }, methods: ["GET", "POST"], allowedHeaders: ["Content-Type"]
+}));
 app.use(express.json());
 
 app.post("/shorten", urlController.shorten.bind(urlController));
